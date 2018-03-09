88ab182ccb9a9a5d5f99e4cb22065d9e0e2e089e
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2018-03-09 19:47:09 +0700

    Added migration for session execution id

--- Ecclesia/src/Ecclesia.DataAccessLayer/Migrations/20180309124402_SessionExecutionIdAdded.Designer.cs
+++ Ecclesia/src/Ecclesia.DataAccessLayer/Migrations/20180309124402_SessionExecutionIdAdded.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Ecclesia.DataAccessLayer.Migrations
 {
     [DbContext(typeof(PsqlContext))]
-    partial class PsqlContextModelSnapshot : ModelSnapshot
+    [Migration("20180309124402_SessionExecutionIdAdded")]
+    partial class SessionExecutionIdAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ecclesia/src/Ecclesia.DataAccessLayer/Migrations/20180309124402_SessionExecutionIdAdded.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Ecclesia.DataAccessLayer.Migrations
+{
+    public partial class SessionExecutionIdAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExecutionId",
+                table: "Sessions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExecutionId",
+                table: "Sessions");
+        }
+    }
+}
