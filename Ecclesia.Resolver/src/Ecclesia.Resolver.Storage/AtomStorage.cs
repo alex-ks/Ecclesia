e32761ed51e66efeb5675189e29370d343d86353
Alexander Komissarov <komissarov.allex@yandex.ru>
2018-04-10 17:29:49 +0700

    Implemented AtomStorage Not tested

--- Ecclesia.Resolver/src/Ecclesia.Resolver.Storage/AtomStorage.cs
+++ Ecclesia.Resolver/src/Ecclesia.Resolver.Storage/AtomStorage.cs
@@ -1,19 +1,87 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using Ecclesia.Resolver.Orm;
+using Ecclesia.Resolver.Orm.Models;
 using Ecclesia.Resolver.Storage.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecclesia.Resolver.Storage
 {
-    public class AtomStorage
+    public class AtomStorage : IDisposable
     {
-        public IEnumerable<AtomId> GetDependencies(AtomId atom)
+        private ResolverContext _context;
+
+        public AtomStorage(ResolverContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public byte[] GetContent(AtomId atom)
+        public IEnumerable<AtomId> GetDependencies(AtomId atomId)
         {
-            throw new NotImplementedException();
+            var atom = 
+                _context.Atoms
+                    .Include(a => a.Dependencies)
+                    .ThenInclude(d => d.Dependency)
+                    .Single(a => a.Kind == atomId.Kind && a.Name == atomId.Name && atomId.Version == a.Version);
+
+            var deps = from dep in atom.Dependencies
+                       select new AtomId
+                       {
+                           Kind = dep.Dependency.Kind,
+                           Name = dep.Dependency.Name,
+                           Version = dep.Dependency.Version
+                       };
+
+            return deps.ToList();
+        }
+
+        public byte[] GetContent(AtomId atomId)
+        {
+            var atom = _context.Atoms.Include(a => a.Content).Single(a => 
+                a.Kind == atomId.Kind && a.Name == atomId.Name && atomId.Version == a.Version);
+            return atom.Content.Content;
+        }
+
+        public async Task AddAsync(AtomId atomId, IEnumerable<AtomId> dependencies, byte[] content)
+        {
+            var dbAtom = new Atom
+            {
+                Kind = atomId.Kind,
+                Name = atomId.Name,
+                Version = atomId.Version,
+            };
+
+            _context.Atoms.Add(dbAtom);
+
+            var depRefs = dependencies.Select(depId => 
+                _context.Atoms.Single(a => 
+                    a.Kind == depId.Kind && a.Name == depId.Name && depId.Version == a.Version));
+            
+            var dbDeps = from dep in depRefs
+                         select new AtomDependency
+                         {
+                             Dependent = dbAtom,
+                             Dependency = dep
+                         };
+
+            _context.AtomDependencies.AddRange(dbDeps);
+
+            var dbContent = new AtomContent
+            {
+                Atom = dbAtom,
+                Content = content
+            };
+
+            _context.AtomContents.Add(dbContent);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
         }
     }
 }
