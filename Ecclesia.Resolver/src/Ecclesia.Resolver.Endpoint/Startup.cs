d2283cd7f69c7c88c953be77fbe6f629eaf703ab
Alexander Komissarov <alex-ks@users.noreply.github.com>
2018-05-15 15:16:25 +0700

    Added auto-generated swagger  API description for Resolver

--- Ecclesia.Resolver/src/Ecclesia.Resolver.Endpoint/Startup.cs
+++ Ecclesia.Resolver/src/Ecclesia.Resolver.Endpoint/Startup.cs
@@ -10,11 +10,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Ecclesia.Resolver.Endpoint
 {
     public class Startup
     {
+        private const string ApiVersion = "v0.1";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +29,9 @@ namespace Ecclesia.Resolver.Endpoint
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c => 
+                c.SwaggerDoc(ApiVersion, new Info { Title = "Ecclesia.Resolver API", Version = ApiVersion }));
 
             var connectionString = Configuration.GetConnectionString("psql");
             services.AddTransient<ResolverContext, NpgsqlResolverContext>(_ => 
@@ -42,6 +48,10 @@ namespace Ecclesia.Resolver.Endpoint
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+                c.SwaggerEndpoint($"/swagger/{ApiVersion}/swagger.json", $"Ecclesia.Resolver API {ApiVersion}"));
 
             app.UseMvc();
         }
